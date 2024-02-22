e2091a70bc50038ee20c25f45bac3159278b960b
dewittbrit <szabobrit@gmail.com>
2024-02-22 18:08:22 -0500

    Redo

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-   
 
-    var flavorsString = prompt("Enter froyo flavors (comma-separated):");
 
-    // Criterion: Functionality
-    // The user's input string is split into an array of strings.
-    var flavorsArray = flavorsString.split(',');
+const userInputString = prompt(
+    "Please enter froyo flavors by commas."
+)
 
-    // Criterion: Functionality
-    // A loop is used to iterate through the array of flavors.
-    for (var i = 0; i < flavorsArray.length; i++) {
-        // Criterion: Functionality
-        // An object is used to keep count of how many orders there are of each flavor.
-       
-        countFlavor(flavorsArray[i]);
+const stringArray = userInputString.split(",");
+
+console.log(stringArray);
+
+function count(stringArray){
+    const map = {};
+    for (let i = 0; i < stringArray.length; i++){
+        if (map[stringArray[i]]){
+            map[stringArray[i]]++;
+        } else {
+            map[stringArray[i]] = 1;
+        }
     }
+    return map;
+}
 
-    // Criterion: Code Style
-    // The logic for counting the frequencies of elements in an array is organized into a function that returns an object.
-    function countFlavor(flavor) {
-        
-
-        console.log(`Order for ${flavor} counted.`);
-    }
-});
+console.log(count(stringArray));
